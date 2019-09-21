b29b8e7de7204262d8efd142dcbe9d363e7400b8
hassan.mubarka <hassan.mubarka@cs104u-4b.acad.fullerton.edu>
2019-09-21 14:42:43 -0700

    random recatngle using #

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // Random Rectangle
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+int main()
+{
+  unsigned seed = time(0); // setting max length of rectangle equal to 40
+  srand(seed);
+  std::string numSign;
+  int max = 40;
+  int myNum;
+  int myRandom = rand();
+  myNum = rand() % max + 1;
+
+  numSign.assign(myNum, '#'); // making random number of #
+
+  std::cout << numSign << '\n'; // making rectangle three high
+  std::cout << numSign << '\n';
+  std::cout << numSign << '\n';
+
+  return 0;
+}
